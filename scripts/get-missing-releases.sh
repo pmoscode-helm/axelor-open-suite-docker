e00746867f72055951d500f43ca75f53d67d3bc6
Peter Motzko <info@pmoscode.de>
2023-08-14 00:19:37 +0200

    feat(ci): add GH Action to find missing releases and deploy them to Dockerhub

---
+++ scripts/get-missing-releases.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+AXELOR_RELEASES=`curl --silent "https://api.github.com/repos/$AXELOR_REPO/releases"  | jq -r .[].tag_name | grep -E 'v6\..\..|v7\..\..|v8\..\..' | sort | tr '\n' ' '`
+PMOSCODE_RELEASES=`curl --silent "https://hub.docker.com/v2/repositories/$PMOSCODE_DOCKER/tags?page_size=1024" | jq -r .results[].name | grep -E 'v6\..\..|v7\..\..|v8\..\..' | sort | tr '\n' ' '`
+
+AXELOR_RELEASES_ARRAY=("${AXELOR_RELEASES}")
+PMOSCODE_RELEASES_ARRAY=("${PMOSCODE_RELEASES}")
+
+echo axelor versions: ${AXELOR_RELEASES_ARRAY}
+echo pmoscode versions: ${PMOSCODE_RELEASES_ARRAY}
+
+MISSING=$(echo ${PMOSCODE_RELEASES_ARRAY[@]} ${AXELOR_RELEASES_ARRAY[@]} | tr ' ' '\n' | sort | uniq -u)
+
+echo " "
+echo Umtagged versions: ${MISSING[@]}
+echo " "
+
+for value in ${MISSING}
+do
+    echo "axelorTag=${value}" >> $GITHUB_OUTPUT
+    # git tag -a ${value} -m "New Version $value"; git push https://oauth2:${REPO_ACCESS_TOKEN}@gitlab.com/pmoscode/axelor-docker.git HEAD:master --tags
+done
