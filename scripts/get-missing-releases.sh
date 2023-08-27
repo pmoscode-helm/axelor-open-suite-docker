208b83a748a24429ec44789baa0708adb366bb51
Peter Motzko <info@pmoscode.de>
2023-08-27 23:16:07 +0200

    fix(script): rewrite version compare logic

--- scripts/get-missing-releases.sh
+++ scripts/get-missing-releases.sh
@@ -5,21 +5,24 @@ set -e
 AXELOR_RELEASES=`curl --silent "https://api.github.com/repos/$AXELOR_REPO/releases"  | jq .[].tag_name | grep -E 'v6\..\..|v7\..\..|v8\..\..' | sort | tr '\n' ' '`
 PMOSCODE_RELEASES=`curl --silent "https://hub.docker.com/v2/repositories/$PMOSCODE_DOCKER/tags?page_size=1024" | jq .results[].name | grep -E 'v6\..\..|v7\..\..|v8\..\..' | sort | tr '\n' ' '`
 
-AXELOR_RELEASES_ARRAY=("${AXELOR_RELEASES}")
-PMOSCODE_RELEASES_ARRAY=("${PMOSCODE_RELEASES}")
+AXELOR_RELEASES_ARRAY=(${AXELOR_RELEASES})
+PMOSCODE_RELEASES_ARRAY=(${PMOSCODE_RELEASES})
 
-echo axelor versions: ${AXELOR_RELEASES_ARRAY}
-echo pmoscode versions: ${PMOSCODE_RELEASES_ARRAY}
+echo axelor versions: ${AXELOR_RELEASES_ARRAY[@]}
+echo pmoscode versions: ${PMOSCODE_RELEASES_ARRAY[@]}
 
-MISSING=$(echo ${PMOSCODE_RELEASES_ARRAY[@]} ${AXELOR_RELEASES_ARRAY[@]} | tr ' ' '\n' | sort | uniq -u)
+for version in "${PMOSCODE_RELEASES_ARRAY[@]}"; do
+  AXELOR_RELEASES_ARRAY=("${AXELOR_RELEASES_ARRAY[@]/$version}")
+  echo "Remove already existing version $version"
+done
 
 echo " "
-echo Umtagged versions: ${MISSING[@]}
+echo Umtagged versions: ${AXELOR_RELEASES_ARRAY[@]}
 echo " "
 
-JSON="[$(echo $MISSING | sed 's/ /,/g')]"
+JSON="[$(echo ${AXELOR_RELEASES_ARRAY[@]} | sed 's/ /,/g')]"
 
-#VAL="$( echo "$JSON" )"
 echo "axelorTag=$( echo "$JSON" )" >> $GITHUB_OUTPUT
 
+#VAL="$( echo "$JSON" )"
 #echo $VAL | jq
