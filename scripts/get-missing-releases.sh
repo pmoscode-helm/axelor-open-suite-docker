441c659b45e13432b97311b7439d9cefd41f5e1d
Peter Motzko <info@pmoscode.de>
2023-09-25 01:05:47 +0200

    feat(ci): add parameter to predefine versions to build

--- scripts/get-missing-releases.sh
+++ scripts/get-missing-releases.sh
@@ -2,27 +2,40 @@
 
 set -e
 
-AXELOR_RELEASES=`curl --silent "https://api.github.com/repos/$AXELOR_REPO/releases"  | jq .[].tag_name | grep -E 'v6\..\..|v7\..\..|v8\..\..' | sort | tr '\n' ' '`
-PMOSCODE_RELEASES=`curl --silent "https://hub.docker.com/v2/repositories/$PMOSCODE_DOCKER/tags?page_size=1024" | jq .results[].name | grep -E 'v6\..\..|v7\..\..|v8\..\..' | sort | tr '\n' ' '`
+PREDEFINED_VERSIONS=$1
 
-AXELOR_RELEASES_ARRAY=(${AXELOR_RELEASES})
-PMOSCODE_RELEASES_ARRAY=(${PMOSCODE_RELEASES})
+if [[ ! -z "$PREDEFINED_VERSIONS" ]]; then
+  RELEASES=(${PREDEFINED_VERSIONS})
+  echo predefined releases to build: ${RELEASES[@]}
 
-echo axelor versions: ${AXELOR_RELEASES_ARRAY[@]}
-echo pmoscode versions: ${PMOSCODE_RELEASES_ARRAY[@]}
+  JSON="[$(echo ${RELEASES[@]} | sed 's/ /,/g')]"
+  echo "axelorTag=$( echo "$JSON" )" >> $GITHUB_OUTPUT
 
-for version in "${PMOSCODE_RELEASES_ARRAY[@]}"; do
-  AXELOR_RELEASES_ARRAY=("${AXELOR_RELEASES_ARRAY[@]/$version}")
-  echo "Remove already existing version $version"
-done
+  #VAL="$( echo "$JSON" )"
+  #echo $VAL | jq
+else
+  AXELOR_RELEASES=`curl --silent "https://api.github.com/repos/$AXELOR_REPO/releases"  | jq .[].tag_name | grep -E 'v7\..\..|v8\..\..' | sort | tr '\n' ' '`
+  PMOSCODE_RELEASES=`curl --silent "https://hub.docker.com/v2/repositories/$PMOSCODE_DOCKER/tags?page_size=1024" | jq .results[].name | grep -E 'v7\..\..|v8\..\..' | sort | tr '\n' ' '`
 
-echo " "
-echo Umtagged versions: ${AXELOR_RELEASES_ARRAY[@]}
-echo " "
+  AXELOR_RELEASES_ARRAY=(${AXELOR_RELEASES})
+  PMOSCODE_RELEASES_ARRAY=(${PMOSCODE_RELEASES})
 
-JSON="[$(echo ${AXELOR_RELEASES_ARRAY[@]} | sed 's/ /,/g')]"
+  echo axelor versions: ${AXELOR_RELEASES_ARRAY[@]}
+  echo pmoscode versions: ${PMOSCODE_RELEASES_ARRAY[@]}
 
-echo "axelorTag=$( echo "$JSON" )" >> $GITHUB_OUTPUT
+  for version in "${PMOSCODE_RELEASES_ARRAY[@]}"; do
+    AXELOR_RELEASES_ARRAY=("${AXELOR_RELEASES_ARRAY[@]/$version}")
+    echo "Remove already existing version $version"
+  done
 
-#VAL="$( echo "$JSON" )"
-#echo $VAL | jq
+  echo " "
+  echo Umtagged versions: ${AXELOR_RELEASES_ARRAY[@]}
+  echo " "
+
+  JSON="[$(echo ${AXELOR_RELEASES_ARRAY[@]} | sed 's/ /,/g')]"
+
+  echo "axelorTag=$( echo "$JSON" )" >> $GITHUB_OUTPUT
+
+  #VAL="$( echo "$JSON" )"
+  #echo $VAL | jq
+fi
